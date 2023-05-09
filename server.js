f3758c93c5c3bba13d06d3df1475aba408417a0a
meiliou <83768277+meiliou@users.noreply.github.com>
2023-05-09 18:03:26 +0400

    added parties and joined both tables

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 // Get all candidates - temporarily commented out to reduce noise in terminal
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -45,8 +49,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
