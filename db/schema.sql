b45e48fd081e84a78cd9bcf2c55cdf13a1d9e9dc
meiliou <83768277+meiliou@users.noreply.github.com>
2023-05-12 11:21:07 +0400

    votes schema, capture votes and summarise vote results

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
 
 CREATE TABLE parties (
   id INTEGER PRIMARY KEY,
@@ -18,11 +19,21 @@ CREATE TABLE candidates (
 );
 
 CREATE TABLE voters (
-    id INTEGER PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
---   zip_code INTEGER NOT NULL,
---   CONSTRAINT zip_code_check CHECK (char_length(zip_code) = 5)
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+--  zip_code INTEGER NOT NULL,
+--  CONSTRAINT zip_code_check CHECK (char_length(zip_code) = 5)
+);
+
+CREATE TABLE votes (
+  id INTEGER PRIMARY KEY,
+  voter_id INTEGER UNSIGNED NOT NULL,
+  candidate_id INTEGER UNSIGNED NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
